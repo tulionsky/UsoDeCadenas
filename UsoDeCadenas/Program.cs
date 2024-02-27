f8f2022236880f26c4e1a0941a9bade00a0bd694
Tulio <tquintanaa@miumg.edu.gt>
2024-02-26 23:23:57 -0600

    Agregar archivos de proyecto.

---
+++ UsoDeCadenas/Program.cs
@@ -0,0 +1,18 @@
+﻿//CONCATENAR CADENAS
+string txt1 = "Hola ";
+string txt2 = "Que tal?";
+string txt3, txt4;
+string nombre = "Pablo";
+int edad = 18;
+
+txt3 = txt1 + txt2; //UTILIZANDO "+"
+Console.WriteLine(txt3);
+
+txt3 = String.Concat(txt1, txt2); //UTILIZANDO STRING.CONCAT
+Console.WriteLine(txt3);
+
+txt4 = string.Format("El nombre de la persona es {0} y su edad es de {1}",nombre,edad); //UTILIZANDO STRING.FORMAT
+Console.WriteLine(txt4);
+
+txt4 = $"EL nombre de la persona es {nombre} y la edad es de {edad}"; //UTILIZANDO INTERPOLACION
+Console.WriteLine(txt4);
