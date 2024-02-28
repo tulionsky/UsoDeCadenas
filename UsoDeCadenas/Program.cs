f57c8b0046bc11d2fbbfe339413cce4cffed4fc5
Tulio <tquintanaa@miumg.edu.gt>
2024-02-27 22:36:37 -0600

    ejercicios

--- UsoDeCadenas/Program.cs
+++ UsoDeCadenas/Program.cs
@@ -1,69 +1,112 @@
-﻿string cadena1 = "Hola";
-string cadena2 = "Hola";
-
-// comparacion
-if (cadena1 == cadena2)
+﻿/* 1. Escribir un programa que le pida al usuario un número entero positivo y le muestre un menú con las siguientes opciones: */
+int opcion;
+do
 {
-    Console.WriteLine("Las cadenas son iguales.");
-}
-else
-{
-    Console.WriteLine("Las cadenas son diferentes.");
-}
+    int numero;
+    do
+    {
+        Console.Write("Ingrese un número entero positivo: ");
+    } while (!int.TryParse(Console.ReadLine(), out numero) || numero <= 0);
+    do
+    {
+        Console.WriteLine("Menú:");
+        Console.WriteLine("1. Calcular el factorial del número.");
+        Console.WriteLine("2. Calcular la raíz cuadrada del número.");
+        Console.WriteLine("3. Salir del programa.");
+        Console.Write("Seleccione una opción: ");
+    } while (!int.TryParse(Console.ReadLine(), out opcion) || opcion < 1 || opcion > 3);
+    switch (opcion)
+    {
+        case 1:
+            long fact = 1;
+            for (int i = 1; i <= numero; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine($"El factorial de {numero} es {fact}.");
+            break;
+        case 2:
+            double raiz = Math.Sqrt(numero);
+            Console.WriteLine($"La raíz cuadrada de {numero} es {raiz}.");
+            break;
+        case 3:
+            Console.WriteLine("¡Ya te cansaste? Bye Bye");
+            break;
+        default:
+            Console.WriteLine("Opción invalida. Por favor seleccione una opción del menú.");
+            break;
+    }
+} while (opcion != 3);
 
-if (cadena1.Equals(cadena2, StringComparison.OrdinalIgnoreCase))
-{
-    Console.WriteLine("Las cadenas son iguales, ignorando mayúsculas y minúsculas.");
-}
-else
-{
-    Console.WriteLine("Las cadenas son diferentes.");
-}
+/* 2. Escribir un programa que solicite al usuario dos números enteros y un operador matemático (+, -, *, /) */
+//int opcion;
+//Console.Write("Ingrese el primer número: ");
+//int.TryParse(Console.ReadLine(), out int numero1);
 
+//Console.Write("Ingrese el segundo número: ");
+//int.TryParse(Console.ReadLine(), out int numero2);
 
-//Concatenacion 
-Console.WriteLine("Por favor, ingresa tu nombre:");
-string nombre = Console.ReadLine();
+//Console.Write("Ingrese el operador (+, -, *, /): ");
+//string entrada = Console.ReadLine();
 
-Console.WriteLine("Por favor, ingresa tu apellido:");
-string apellido = Console.ReadLine();
+//double resultado = 0;
+//switch (entrada)
+//{
+//    case "+":
+//        resultado = numero1 + numero2;
+//        break;
+//    case "-":
+//        resultado = numero1 - numero2;
+//        break;
+//    case "*":
+//        resultado = numero1 * numero2;
+//        break;
+//    case "/":
+//        if (numero2 == 0)
+//        {
+//            Console.WriteLine("No se puede dividir por cero.");
+//            return;
+//        }
+//        resultado = (double)numero1 / numero2;
+//        break;
+//    default:
+//        Console.WriteLine("Esa operacion es invalida.");
+//        return;
+//}
+//Console.WriteLine($"El resultado de la operación es: {resultado}");
 
-Console.WriteLine("Por favor, ingresa tu edad:");
-int edad = int.Parse(Console.ReadLine());
+/* 3. Escribir un programa que imprima una tabla de multiplicar del 1 al 10 para un número introducido por el usuario. */
+//int opcion;
 
-Console.WriteLine("Hola " + nombre + " " + apellido + ", tienes " + edad + " años.");
-Console.WriteLine($"Hola {nombre} {apellido}, tienes {edad} años.");
+//Console.Write("Ingrese el número del que desea ver la tabla: ");
+//int.TryParse(Console.ReadLine(), out int numero);
 
-// Busqueda de cadenas
-string texto = "Paciencia debes tener, mi joven padawan.";
-string patron = "padawan";
+//Console.WriteLine($"Tabla de multiplicar de {numero} es:");
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine($"{numero} x {i} = {numero * i}");
+//}
 
-int indice = texto.IndexOf(patron);
+/* 4. Escribir un programa que adivine un número secreto entre 1 y 100.  */
+//Random num = new Random();
+//int numAl= num.Next(1, 101);
+//int intento;
 
-if (indice != -1)
-{
-    Console.WriteLine($"La palabra {patron} fue encontrada en el índice {indice}");
-}
-else
-{
-    Console.WriteLine("No se encontraron coincidencias.");
-}
+//Console.WriteLine("¡Adivina el número entre 1 y 100!");
 
-if (texto.Contains(patron))
-{
-    Console.WriteLine($"Se encontró el patrón \"{patron}\" en el texto.");
-}
-else
-{
-    Console.WriteLine("No se encontraron coincidencias.");
-}
-//Formateo de cadenas
-string nombre1 = "Luke";
-string resultado = $"{nombre1}, Yo soy tu padre!!";
-Console.WriteLine(resultado);
+//do
+//{
+//    Console.Write("Introduce tu intento: ");
+//    intento = int.Parse(Console.ReadLine());
 
-string nombre2 = "Anakin";
-string nombre3 = "Skywalker";
-string resultado1=string.Format("Yo no soy {0} {1},{0} esta muerto.", nombre2, nombre3);
-Console.WriteLine(resultado1);
+//    if (intento < numAl)
+//    {
+//        Console.WriteLine("El número es mayor. Bajale un poquito");
+//    }
+//    else if (intento > numAl)
+//    {
+//        Console.WriteLine("El número es menor. Dale un poco mas");
+//    }
+//} while (intento != numAl);
 
+//Console.WriteLine("¡ESOOO! ¡Has adivinado el número " + numAl + "!");
